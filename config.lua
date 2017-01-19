d216c8cd3c0cec8be6a75bb4dea5a97f8e51beb3
roboxt <robo.xt7aj@gmail.com>
2017-01-19 21:43:53 +0330

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,12 +1,12 @@
 return {
-	bot_api_key = "", --Please add your bot api key here!
+	bot_api_key = "211738311:AAHDkbJXAhx9-5aBW_GlxqO0hx3xPGEBPi4", --Please add your bot api key here!
 	cmd = '^[/!#]',
 	allowed_updates = {"message", "edited_message", "callback_query"},
 	db = 2, --default redis db: 0
-	superadmins = {23646077, 278941742},
+	superadmins = {186280031, 186280031},
 	log = {
 		chat = -1001089188566, --Your log chat, where your bot must be added!
-		admin = 23646077, --The admin.
+		admin = 186280031, --The admin.
 		stats = nil
 	},
 	human_readable_version = '4.2.0',
